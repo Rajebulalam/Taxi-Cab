2749db5f2cbc45568f9c8ccb35f3db12eb42ea14
unknown <rajebul.alam17@gmail.com>
2022-06-01 19:20:35 +0600

    Focus Part Designed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Focus from './Focus';
 
 const Home = () => {
     return (
         <section>
             <Banner></Banner>
+            <Focus></Focus>
         </section>
     );
 };
